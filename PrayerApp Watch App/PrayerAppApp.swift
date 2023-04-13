8d82bc6f704a07de0626b33f1c0b096cf8bc81f3
Sharan Thakur <sharandt19@live.com>
2023-04-13 15:21:43 +0530

    Initial Commit

---
+++ PrayerApp Watch App/PrayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerAppApp.swift
+//  PrayerApp Watch App
+//
+//  Created by Sharan Thakur on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PrayerApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
